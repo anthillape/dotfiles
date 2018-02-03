6e0a9e9f8708f671c3111ddabab3152db441cc53
Arthur Comben <anthillape@gmail.com>
2018-02-03 10:05:57 +0000

    Update readme to explain that this is a work in progress
    Update install script to install cask and iterm2

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dotfiles
 My dotfiles
+
+This is currently a work in progress and does not currently function fully.
+
+For now this includes some basic tmux config and the beginnings of an install
+script. Eventually I hope to be able to provision a mac with a single command.
+
--- brew.sh
+++ brew.sh
@@ -4,7 +4,16 @@ echo "Installing brew"
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install cask, which installs non CLI apps
+# https://caskroom.github.io/
 
+brew tap caskroom/cask
+brew install cask
+
+# Install terminal emulator iterm2 
+# https://www.iterm2.com/
+
+brew cask install iterm2
 
 # This wrapper around tmux allows the use of pbcopy and pbpaste
 brew install reattach-to-user-namespace
