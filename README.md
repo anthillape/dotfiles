b81f7d894e82a97fc4e95fab913ba287dbacd8b7
Arthur Comben <anthillape@gmail.com>
2018-02-03 11:28:38 +0000

    Add many more cask recipes, update readme

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ This is currently a work in progress and does not currently function fully.
 For now this includes some basic tmux config and the beginnings of an install
 script. Eventually I hope to be able to provision a mac with a single command.
 
+I intend to document every step heavily so even people new to the command line
+are able to understand what's going on (and more to the point myself when 
+trying to edit any of this later)
--- brew.sh
+++ brew.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# The aim of this script is to be able to run it once to set up a mac
+# and again at any time to update
+
+# Many thanks to:
+# Deian Isac https://medium.com/@deianisac/how-to-quickly-set-up-a-mac-b9f89aed0dc 
+
 # Test to see if homebrew is installed, if not install it
 if test ! $(which brew)
 then
@@ -7,25 +13,52 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
 fi
 
+# Update recipies
+brew update
+
+# Update installed formulae
+brew upgrade --all
+
 # Install cask, which installs non CLI apps
-# https://caskroom.github.io/
 
 brew tap caskroom/cask
 brew install cask
 
 # Install mas, an app store CLI interface
-# https://github.com/mas-cli/mas
 brew install mas
 
-# Install some utility programs
-
+# Install wget, for downloading files over http/s or ftp 
 brew install wget
 
+# Install vlc media player
+brew install cask vlc
 
 # Install terminal emulator iterm2 
-# https://www.iterm2.com/
-
 brew cask install iterm2
+
+# Install password manager
+brew cask install 1password
+
+# Install caffeine, an app that stops your mac sleeping while active
+brew cask install caffeine
+
+# Install cakebrew, a handy GUI for homebrew
+brew cask install cakebrew
+
+# Install dropbox, for managing files across devices
+brew cask install dropbox
+
+# Install flux, an app that warms screen colours at night to aid sleep
+brew cask install flux
+
+# Install imageoptim, an image file size reducer (reduces metadata etc)
+brew cask install imageoptim
+
+# Install kapako, an ambient sound mixer / player
+brew cask install kapako
+
+# Install little snitch, to keep an eye on your network
+brew cask install little-snitch
 
 # Install some browsers for testing
 brew cask install firefox
@@ -34,3 +67,6 @@ brew cask install google-chrome
 # This wrapper around tmux allows the use of pbcopy and pbpaste
 brew install reattach-to-user-namespace
 
+# Cleanup brew and cask
+brew cleanup
+brew cask cleanup
