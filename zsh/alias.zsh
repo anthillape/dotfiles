180957ca4a1887bdefb133458609b3a402889c37
Arthur Comben <anthillape@gmail.com>
2018-04-10 11:18:37 +0100

    Update l alias to show single line output

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,5 +1,5 @@
 
-# Whenever we open tmux we want to make sure it sets the TERM variable that is 
+# Whenever we open tmux we want to make sure it sets the TERM variable that is
 # used for tmux, rather than inherit the one used for iterm2, ie xterm-256color
 # we only want this for tmux, and not iterm2, hence the use of an alias
 # rather than export
@@ -7,9 +7,12 @@ alias term='TERM=screen-256color tmux'
 
 alias reload=". ~/.zshrc && echo 'zsh config reloaded'"
 
-alias l='ls -al'
-alias ls='ls -GFh' # Colorize output, add file type indicator, and put sizes in human readable format
-alias ll='ls -GFhl' # Same as above, but in long listing format
+# All entries except dots, single column, file type indicator, colourised
+alias l='ls -A1FG'
+# Colorize output, add file type indicator, and put sizes in human readable format
+alias ls='ls -GFh'
+# Same as above, but in long listing format
+alias ll='ls -GFhl'
 
 alias vi='nvim'
 alias vim='nvim'
